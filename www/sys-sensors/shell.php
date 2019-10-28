96b223ffab810805c34605e0e71055723b3611e9
MissKittin <56042452+MissKittin@users.noreply.github.com>
2019-10-28 16:16:28 +0100

    Update shell.php

--- www/sys-sensors/shell.php
+++ www/sys-sensors/shell.php
@@ -3,5 +3,7 @@
 	if(isset($_GET['sensors']))
 		echo '<pre>' . shell_exec($system_location_php . strtok(str_replace('shell.php', '', $_SERVER['REQUEST_URI']), '?') . '/shell.sh sensors') . '</pre>';
 	else
+	{
 		include($system_location_php . '/lib/prevent-direct.php'); prevent_direct('shell.php');
+	}
 ?>
