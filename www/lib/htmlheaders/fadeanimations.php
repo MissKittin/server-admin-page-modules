80f308d3f85a6809744502e452d3ca8160177161
MissKittin <56042452+MissKittin@users.noreply.github.com>
2019-11-04 18:46:26 +0100

    fade animations v1

---
+++ www/lib/htmlheaders/fadeanimations.php
@@ -0,0 +1,48 @@
+<?php
+	// fade-in/out animations
+	// 04.11.2019
+?>
+<style>
+	body {
+		opacity: 0;
+	}
+</style>
+<!--[if !IE]><!--><script>
+	/* load animation */
+	document.addEventListener('DOMContentLoaded', function(){
+		var bodyOpacity=0;
+		function animate()
+		{
+			setTimeout(function(){
+				if(bodyOpacity < 1)
+				{
+					bodyOpacity=bodyOpacity+0.08;
+					document.body.style.opacity=bodyOpacity;
+					animate();
+				}
+				else
+					document.body.style.opacity=1;
+			}, 1);
+		}
+		animate();
+	});
+
+	/* exit animation */
+	window.onbeforeunload=function(){
+		var bodyOpacity=1;
+		function animate()
+		{
+			setTimeout(function(){
+				if(bodyOpacity > 0.01)
+				{
+					bodyOpacity=bodyOpacity-0.05;
+					document.body.style.opacity=bodyOpacity;
+					animate();
+				}
+				else
+					document.body.style.opacity=0;
+			}, 1);
+		}
+		animate();
+	};
+</script><!--<![endif]-->
